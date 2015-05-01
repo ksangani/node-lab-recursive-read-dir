1dd471bbbac82e5940c4fbb3fdd8ca23e8dae935
Kamlesh Sangani <ksangani@walmartlabs.com>
2015-04-30 18:33:57 -0700

    Recursive read dir

---
+++ index.js
@@ -0,0 +1,37 @@
+let http = require('http')
+let fs = require('fs')
+let path = require('path')
+let url = require('url')
+let _ = require('lodash')
+require('songbird')
+
+
+http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    let parsedUrl = url.parse(req.url)
+
+    console.log(parsedUrl.pathname)
+
+    ls(parsedUrl.pathname)
+        .then(files =>  {
+            console.log(files.length)
+            res.end(JSON.stringify(files))
+        })
+        .catch(e => console.log(e.stack))
+
+}).listen(8080)
+
+
+async function ls(dir){
+    let promises = []
+    let stat = await fs.promise.stat(dir)
+    if(stat.isDirectory()) {
+        for (let file of await fs.promise.readdir(dir)) {
+            promises.push(await ls(path.join(dir, file)))
+        }
+        let results = await Promise.all(promises)
+        return _.flatten(results)
+    } else {
+        return new Array(dir)
+    }
+}
